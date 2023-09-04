e8e15fa40d31ac4a74057aae5c1f155364a90fa5
João Astolfo <joao.astolfo@lnls.br>
2023-09-04 18:14:11 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# ADA-Locate-Car
+Projeto de Sistema de Cadastro de Locadora de Carros para o módulo de POO-II do Santander Coders 2023.
---
+++ src/Veiculo.java
@@ -0,0 +1,13 @@
+public class Veiculo {
+    String modelo;
+    String marca;
+    String cor;
+    String placa;
+
+    public Veiculo(String modelo, String marca, String cor, String placa) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.cor = cor;
+        this.placa = placa;
+    }
+}
