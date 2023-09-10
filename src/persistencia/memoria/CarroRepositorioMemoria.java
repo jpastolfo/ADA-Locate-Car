5d105f348f6be81d57c75c26c91fd4e1a7854fc2
PedroQueiroz1 <pedroqueirozdev@outlook.com>
2023-09-10 16:09:59 -0300

    Remove codigo desnecessario dos rep em memoria

--- src/persistencia/memoria/CarroRepositorioMemoria.java
+++ src/persistencia/memoria/CarroRepositorioMemoria.java
@@ -3,33 +3,8 @@ package persistencia.memoria;
 import modelo.Carro;
 import persistencia.CarroRepositorio;
 
+import java.util.List;
+
 public class CarroRepositorioMemoria extends RepositorioMemoriaAbstrato<Carro, Integer> implements CarroRepositorio {
-
-    @Override
-    public Carro buscarCarroPorId(Integer id) {
-        for (Carro carro : listarTodos()) {
-            if (carro.getId().equals(id)) {
-                return carro;
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public boolean removerCarroPorId(Integer id) {
-        Carro carroRemovido = null;
-        for (Carro carro : listarTodos()) {
-            if (carro.getId().equals(id)) {
-                carroRemovido = carro;
-                break;
-            }
-        }
-        if (carroRemovido != null) {
-            remover(carroRemovido);
-            return true;
-        } else {
-            return false;
-        }
-    }
 
 }
--- src/persistencia/memoria/ClienteJuridicoRepositorioMemoria.java
+++ src/persistencia/memoria/ClienteJuridicoRepositorioMemoria.java
@@ -4,15 +4,4 @@ import modelo.ClienteJuridico;
 import persistencia.ClienteJuridicoRepositorio;
 
 public class ClienteJuridicoRepositorioMemoria extends RepositorioMemoriaAbstrato<ClienteJuridico, Integer> implements ClienteJuridicoRepositorio {
-
-    @Override
-    public ClienteJuridico buscarClienteJuridicoPorId(Integer id) {
-        for (ClienteJuridico cliente : listarTodos()) {
-            if (cliente.getId().equals(id)) {
-                return cliente;
-            }
-        }
-        return null;
-    }
-
 }
