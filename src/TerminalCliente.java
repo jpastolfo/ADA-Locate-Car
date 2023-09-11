31bf958b7e030a7eb05cfbb5ee3d5d33a34e1fa7
PedroQueiroz1 <pedroqueirozdev@outlook.com>
2023-09-11 01:36:28 -0300

    Adiciona constantes

--- src/TerminalCliente.java
+++ src/TerminalCliente.java
@@ -1,15 +1,17 @@
-import modelo.Carro;
 import modelo.ClienteFisico;
 import modelo.ClienteJuridico;
-import modelo.Tamanho;
-import persistencia.memoria.CarroRepositorioMemoria;
 import persistencia.memoria.ClienteFisicoRepositorioMemoria;
 import persistencia.memoria.ClienteJuridicoRepositorioMemoria;
-import servico.CarroServico;
 import servico.ClienteFisicoServico;
 import servico.ClienteJuridicoServico;
 
 public class TerminalCliente {
+
+    private static final String CADASTRO_CLIENTE_FISICO_MESSAGEM = "\n********************** \nCadastrando Cliente Fisico %d...";
+    private static final String BUSCA_CLIENTE_FISICO_MESSAGEM = "\n********************** \nBuscando Cliente Fisico de id %d";
+    private static final String CADASTRO_CLIENTE_JURIDICO_MESSAGEM = "\n********************** \nCadastrando Cliente Juridico %d...";
+    private static final String BUSCA_CLIENTE_JURIDICO_MESSAGEM = "\n********************** \nBuscando Cliente Juridico de id %d";
+
 
     public static void main(String[] args) {
 
@@ -27,18 +29,18 @@ public class TerminalCliente {
                 "Rua das Camelias, 654, São Bernardo, São Paulo", null, "954.654.655-56");
 
 
-        System.out.println("\n********************** \nCadastrando Cliente Fisico 1...");
+        System.out.println(String.format(CADASTRO_CLIENTE_FISICO_MESSAGEM, 1));
         clienteFisicoServico.cadastrar(clienteFisico1);
 
-        System.out.println("\n********************** \nCadastrando Cliente Fisico 2...");
+        System.out.println(String.format(CADASTRO_CLIENTE_FISICO_MESSAGEM, 2));
         clienteFisicoServico.cadastrar(clienteFisico2);
 
-        System.out.println("\n********************** \nCadastrando Cliente Fisico 3...");
+        System.out.println(String.format(CADASTRO_CLIENTE_FISICO_MESSAGEM, 3));
         clienteFisicoServico.cadastrar(clienteFisico3);
         // Buscar cliente físico por id
 
         try {
-            System.out.println("\n********************** \nBuscando Cliente Fisico de id 1");
+            System.out.println(String.format(BUSCA_CLIENTE_FISICO_MESSAGEM, 1));
             ClienteFisico clienteFisicoBuscado1 = clienteFisicoServico.buscarPorId(1);
             System.out.println("ClienteFisico encontrado! \n id: " + clienteFisicoBuscado1.getId());
         } catch (Exception e) {
@@ -46,7 +48,7 @@ public class TerminalCliente {
         }
 
         try {
-            System.out.println("\n********************** \nBuscando Cliente Fisico de id 2");
+            System.out.println(String.format(BUSCA_CLIENTE_FISICO_MESSAGEM, 2));
             ClienteFisico clienteFisicoBuscado2 = clienteFisicoServico.buscarPorId(2);
             System.out.println("Carro encontrado! \n id: " + clienteFisicoBuscado2.getId());
         } catch (Exception e) {
@@ -63,22 +65,22 @@ public class TerminalCliente {
         ClienteJuridico clienteJuridico2 = new ClienteJuridico(2,"Dell Ltda",
                 "Rua Fernando Guimaraes, 484, Marilia, SP", null, "12.232.343/3434-35");
 
-        ClienteJuridico clienteJuridico3 = new ClienteJuridico(2,"Dell Ltda",
+        ClienteJuridico clienteJuridico3 = new ClienteJuridico(3,"Dell 3 Ltda",
                 "Rua Fernando Guimaraes, 484, Marilia, SP", null, "12.232.343/3434-35");
 
 
-        System.out.println("\n********************** \nCadastrando Cliente Juridico 1...");
+        System.out.println(String.format(CADASTRO_CLIENTE_JURIDICO_MESSAGEM, 1));
         clienteJuridicoServico.cadastrar(clienteJuridico1);
 
-        System.out.println("\n********************** \nCadastrando carro 2...");
+        System.out.println(String.format(CADASTRO_CLIENTE_JURIDICO_MESSAGEM, 2));
         clienteJuridicoServico.cadastrar(clienteJuridico2);
 
-        System.out.println("\n********************** \nCadastrando carro 2...");
+        System.out.println(String.format(CADASTRO_CLIENTE_JURIDICO_MESSAGEM, 3));
         clienteJuridicoServico.cadastrar(clienteJuridico3);
 
         // Buscar cliente jurídico por id
         try {
-            System.out.println("\n********************** \nBuscando Cliente Juridico de id 1");
+            System.out.println(String.format(BUSCA_CLIENTE_JURIDICO_MESSAGEM, 1));
             ClienteJuridico clienteJuridicoBuscado1 = clienteJuridicoServico.buscarPorId(1);
             System.out.println("ClienteJuridico encontrado! \n id: " + clienteJuridicoBuscado1.getId());
         } catch (Exception e) {
@@ -86,7 +88,7 @@ public class TerminalCliente {
         }
 
         try {
-            System.out.println("\n********************** \nBuscando Cliente Juridico de id 2");
+            System.out.println(String.format(BUSCA_CLIENTE_JURIDICO_MESSAGEM, 2));
             ClienteJuridico clienteJuridicoBuscado2 = clienteJuridicoServico.buscarPorId(2);
             System.out.println("Carro encontrado! \n id: " + clienteJuridicoBuscado2.getId());
         } catch (Exception e) {
